ffcdbd8b727c183c93df023581fd8ddf27759c38
Anthony Lu <lua@mit.edu>
2014-05-21 10:04:15 -0400

    Visualize ground truth data

--- viz.py
+++ viz.py
@@ -1,8 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import matplotlib.animation as animation
 import mpl_toolkits.basemap as basemap
 
+from collections import Counter
+from utils import *
+from model import *
+
+import venture.shortcuts as s
+ripl = s.make_puma_church_prime_ripl()
+
+cell_map = [
+    0, 0, 0, 1, 2, 2, 2, 2, 3, 3,
+    0, 0, 0, 1, 2, 2, 2, 2, 3, 3,
+    4, 0, 0, 1, 1, 2, 2, 2, 3, 3,
+    4, 5, 5, 1, 1, 2, 2, 2, 3, 3,
+    4, 5, 5, 1, 1, 2, 2, 2, 3, 8,
+    4, 5, 5, 6, 7, 7, 8, 8, 8, 8,
+    4, 5, 6, 6, 7, 7, 8, 8, 8, 8,
+    4, 5, 6, 6, 7, 7, 8, 8, 9, 9,
+    4, 5, 6, 6, 7, 7, 8, 9, 9, 9,
+    5, 6, 6, 6, 7, 7, 8, 9, 9, 9,
+]
 regions = {
     'Mississippi': 0,
     'Alabama': 0,
@@ -38,6 +58,7 @@ vectors = {
     (0, 4): (-86, 31, -30),
     (0, 5): (-85, 33, 0),
     (4, 5): (-83, 31, 120),
+    (1, 5): (-85, 35, -90),
     (1, 6): (-82, 36, -30),
     (5, 6): (-82, 34, 30),
     (1, 7): (-82, 38, 30),
@@ -45,6 +66,7 @@ vectors = {
     (6, 7): (-80, 37, 90),
     (2, 8): (-80, 41, 0),
     (7, 8): (-78, 40, 60),
+    (3, 8): (-81, 43, -30),
     (8, 9): (-73, 43, 30),
 }
 
@@ -71,15 +93,44 @@ def plot_map(values, vmin, vmax, flows):
         plt.fill(xx, yy, color, edgecolor=color)
     xs = []
     for from_cell, to_cell, num_birds in flows:
+        if num_birds == 0:
+            continue
         if vectors.has_key((from_cell, to_cell)):
             lon, lat, angle = vectors[(from_cell, to_cell)]
             xs.append((lon, lat, angle, num_birds))
         elif vectors.has_key((to_cell, from_cell)):
             lon, lat, angle = vectors[(to_cell, from_cell)]
             xs.append((lon, lat, angle, -num_birds))
-    lons, lats, angles, nums_birds = np.transpose(xs)
-    u = np.cos(angles*np.pi/180) * np.log1p(np.abs(nums_birds)) * np.sign(nums_birds)
-    v = np.sin(angles*np.pi/180) * np.log1p(np.abs(nums_birds)) * np.sign(nums_birds)
-    m.quiver(lons, lats, u, v, latlon=True,
-             pivot='middle', zorder=2, color='#3333ff')
-    plt.show()
+    if xs:
+        lons, lats, angles, nums_birds = np.transpose(xs)
+        u = np.cos(angles*np.pi/180) * np.log1p(np.abs(nums_birds)) * np.sign(nums_birds)
+        v = np.sin(angles*np.pi/180) * np.log1p(np.abs(nums_birds)) * np.sign(nums_birds)
+        m.quiver(lons, lats, u, v, latlon=True,
+                 pivot='middle', zorder=2, color='#3333ff')
+    return m
+
+def get_data():
+    observations = readObservations('release/10x10x1000-train-observations.csv')
+    bird_moves = readReconstruction(2)
+    meta_observations = Counter()
+    for y, yobs in observations.iteritems():
+        for d, dobs in enumerate(yobs):
+            for i, n in enumerate(dobs[1]):
+                i = cell_map[i]
+                meta_observations[(y, d, i)] += n
+
+    bird_meta_moves = Counter()
+    for (y, d, i, j), n in bird_moves.iteritems():
+        i = cell_map[i]
+        j = cell_map[j]
+        if i == j: pass
+        if i > j:
+            i, j = j, i
+            n = -n
+        bird_meta_moves[(y, d, i, j)] += n
+    def data(y, d):
+        obs = [meta_observations[y, d, i] for i in xrange(10)]
+        flows = [(i, j, bird_meta_moves[y, d, i, j])
+                 for i in xrange(10) for j in xrange(10)]
+        return (obs, 0, 1000, flows)
+    return data
